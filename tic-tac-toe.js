2ee238c3d31df4ec34ec2a33710edae0a1a75280
alyanaduncan50 <alyanaduncan1@gmail.com>
2024-10-24 11:07:14 -0500

    Added hover effect

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,17 +6,30 @@ document.addEventListener('DOMContentLoaded', () => {
     square.classList.add('square');
   });
 
-let currentPlayer = 'X';
-
-squares.forEach(square => {
-  square.addEventListener('click', () => {
-    if (square.textContent === '' && !document.getElementById('status').classList.contains('you-won')) {
-      square.textContent = currentPlayer;
-      square.classList.add(currentPlayer);
-
-      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-    }
+  let currentPlayer = 'X';
+  
+  squares.forEach(square => {
+    square.addEventListener('click', () => {
+      if (square.textContent === '' && !document.getElementById('status').classList.contains('you-won')) {
+        square.textContent = currentPlayer;
+        square.classList.add(currentPlayer);
+  
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+      }
+    });
   });
-});
+
+    // Hover event handling
+    squares.forEach(square => {
+    square.addEventListener('mouseover', () => {
+      if (square.textContent === '') {
+        square.classList.add('hover');
+      }
+    });
+  
+    square.addEventListener('mouseout', () => {
+      square.classList.remove('hover');
+    });
+  });
 
 });
