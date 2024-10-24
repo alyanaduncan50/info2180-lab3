e1f411233eec85aeb56d66a95395d1be4b15c1b6
alyanaduncan50 <alyanaduncan1@gmail.com>
2024-10-24 11:05:47 -0500

    Added functionality to alternate X and O when a square is clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,4 +5,18 @@ document.addEventListener('DOMContentLoaded', () => {
   squares.forEach(square => {
     square.classList.add('square');
   });
+
+let currentPlayer = 'X';
+
+squares.forEach(square => {
+  square.addEventListener('click', () => {
+    if (square.textContent === '' && !document.getElementById('status').classList.contains('you-won')) {
+      square.textContent = currentPlayer;
+      square.classList.add(currentPlayer);
+
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+  });
+});
+
 });
